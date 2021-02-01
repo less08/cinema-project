f94ab583db4c7adf182369dd5c298587d3af0257
Elena Samsonova <elenasamsonova08@gmail.com>
2021-02-01 22:55:00 +0200

    hb-04 task done

---
+++ src/main/java/com/dev/cinema/dao/CinemaHallDaoImpl.java
@@ -0,0 +1,45 @@
+package com.dev.cinema.dao;
+
+import com.dev.cinema.exception.DataProcessException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.CinemaHall;
+import com.dev.cinema.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class CinemaHallDaoImpl implements CinemaHallDao {
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(cinemaHall);
+            transaction.commit();
+            return cinemaHall;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessException("Can`t add cinemaHall " + cinemaHall, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<CinemaHall> query = session.createQuery("from CinemaHall", CinemaHall.class);
+            return query.list();
+        } catch (Exception e) {
+            throw new DataProcessException("Can't get all cinema halls", e);
+        }
+    }
+}
---
+++ src/main/java/com/dev/cinema/service/MovieSessionServiceImpl.java
@@ -0,0 +1,24 @@
+package com.dev.cinema.service;
+
+import com.dev.cinema.dao.MovieSessionDao;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.MovieSession;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class MovieSessionServiceImpl implements MovieSessionService {
+    @Inject
+    private MovieSessionDao movieSessionDao;
+
+    @Override
+    public MovieSession add(MovieSession session) {
+        return movieSessionDao.add(session);
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        return movieSessionDao.findAvailableSessions(movieId, date);
+    }
+}
