f5871820f1331ca50534b936dc4791113cf7ccf4
Elena Samsonova <elenasamsonova08@gmail.com>
2021-02-02 22:26:49 +0200

    hb-05 task done

---
+++ src/main/java/com/dev/cinema/service/AuthenticationServiceImpl.java
@@ -0,0 +1,35 @@
+package com.dev.cinema.service;
+
+import com.dev.cinema.exception.AuthenticationException;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HashUtil;
+import java.util.Optional;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isPresent()) {
+            String hashedPassword = HashUtil.hashPassword(password, user.get().getSalt());
+            if (hashedPassword.equals(user.get().getPassword())) {
+                return user.get();
+            }
+        }
+        throw new AuthenticationException("Can't authenticate by email " + email
+            + " and password " + password);
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return userService.add(user);
+    }
+}
